9a321d4aa45cc8b1acca435d81997881ee5c427a
Pedro Henrique Chagas <107327241+PhChags@users.noreply.github.com>
2022-08-27 11:27:04 -0300

    Add files via upload

---
+++ Lista01/src/main/java/com/mycompany/lista01/Exercicio04.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lista01;
+
+/**
+ *
+ * @author Pedro
+ */
+public class Exercicio04 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++){
+            if(i % 3 == 0) 
+                System.out.print(" " + i + " ");
+        }
+    }
+}
